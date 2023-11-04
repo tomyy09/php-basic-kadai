7224bfc8e467d2eb1a671a02478db61c2f5a4bab
tomyy09 <144043091+tomyy09@users.noreply.github.com>
2023-11-05 00:35:44 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,15 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+echo $score_total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+echo '<br>';
+echo $score_average = 625 / 10;
